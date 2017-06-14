a2ecec83f2761b905a6f6bb3482a1220891f5987
ilanolkies <ilan@kinetica-solutions.com>
2017-06-14 11:08:24 -0300

    Ejercicio 3 Agregada matriz de relaciones de 5x5

--- tp.hs
+++ tp.hs
@@ -1,22 +1,21 @@
 import Datos
 
---Que dados dos agentes, indique cu´al es su relaci´on.
+--1. Que dados dos agentes, indique cu´al es su relaci´on.
 relacion :: Relaciones -> Agente -> Agente -> Float
-relacion m_relaciones agenteN agenteM = relacionAgenteNM (relacionesAgenteN m_relaciones agenteN 1) agenteM 1
+relacion relaciones agenteN agenteM = relacionAgenteNM (relacionesAgenteN relaciones agenteN 1) agenteM 1
 
 --Auxiliares:
 --Recibe el agente y devuelve la fila de sus relaciones.
-relacionesAgenteN :: Relaciones -> Agente -> Integer -> [Relacion]
+relacionesAgenteN :: Relaciones -> Agente -> Integer -> Set Relacion
 relacionesAgenteN relaciones agenteN pos | pos == agenteN = head relaciones
                                            | pos < agenteN = relacionesAgenteN (tail relaciones) agenteN (pos+1)
 
 --Recibe una lista de relaciones de un agente y devuelve su relacion con otro agente
-relacionAgenteNM :: [Relacion] -> Agente -> Integer -> Relacion
+relacionAgenteNM :: Set Relacion -> Agente -> Integer -> Relacion
 relacionAgenteNM relacionesN agente pos | pos == agente = head relacionesN
                                         | pos < agente = relacionAgenteNM (tail relacionesN) agente (pos+1)
 
---
---Que dado un agente, el n´umero total de agentes del sistema y un estado determinado, indique 
+--2. Que dado un agente, el n´umero total de agentes del sistema y un estado determinado, indique 
 --el conjunto de agentes enemigos.
 enemigos :: Agente -> Integer -> Estado -> Set Agente
 enemigos agente cantidadAgentes estado | agentePertenece estado agente = agentesEnemigos estado agentes
@@ -25,21 +24,30 @@ enemigos agente cantidadAgentes estado | agentePertenece estado agente = agentes
 --Si estan ordenados!!!
 
 --Auxiliares:
---Recibe un estado y devuelve si el agente pertenece a ese estado
+--Recibe un estado y devuelve si el agente pertenece a e se estado
 agentePertenece :: Estado -> Agente -> Bool
-agentePertenece estado agente | length estado == 0 = False
-                              | head estado == agente = True
+agentePertenece [] _ = False
+agentePertenece estado agente | head estado == agente = True
                               | otherwise = agentePertenece (tail estado) agente
 
---compA una funcion auxiliar, para comp
+--Recibe un estado y devuelve el otro bando
 agentesEnemigos :: Estado -> Set Agente -> Estado
-agentesEnemigos estado agentes | length agentes == 0 = []
-                               | length estado == 0 = agentes
-                               | head estado == head agentes = agentesEnemigos (tail estado)  (tail agentes)
+agentesEnemigos _ [] = []
+agentesEnemigos [] agentes = agentes
+agentesEnemigos estado agentes | head estado == head agentes = agentesEnemigos (tail estado)  (tail agentes)
                                | otherwise = [head agentes] ++ agentesEnemigos estado (tail agentes)
 
+--3. Que dado un agente, devuelve su frustracion que es la suma de los valores de su relaci´on con cada uno de los
+--agentes del otro bando.
 frustracion :: Agente -> Relaciones -> Estado -> Frustracion
-frustracion = undefined
+frustracion agente relaciones estado = sumatoriaFrustracion (relacionesAgenteN  relaciones agente 1) (enemigos agente (cantAgentes) estado)
+                                       where cantAgentes = toInteger (length relaciones)
+
+--Auxiliares:
+--Recibe las relaciones de un agente y sus enemigos y suma la relacion con cada uno de ellos
+sumatoriaFrustracion :: Set Relacion -> Estado -> Frustracion
+sumatoriaFrustracion relacionesN [] = 0
+sumatoriaFrustracion relacionesN enemigos = relacionAgenteNM relacionesN (head enemigos) 1 + sumatoriaFrustracion relacionesN (tail enemigos)
 
 energia :: Relaciones -> Estado -> Energia
 energia = undefined
