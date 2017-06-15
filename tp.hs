7f6af4714626b576867cb257a42936acb005f6ad
lukavner <lukavner@gmail.com>
2017-06-15 17:02:13 -0300

    6. esEstable, y puse unos espacios para q las guardas queden alineadas

--- tp.hs
+++ tp.hs
@@ -8,20 +8,20 @@ relacion relaciones agenteN agenteM = relacionAgenteNM (relacionesAgenteN relaci
 --Recibe el agente y devuelve la fila de sus relaciones.
 relacionesAgenteN :: Relaciones -> Agente -> Integer -> Set Relacion
 relacionesAgenteN relaciones agenteN pos | pos == agenteN = head relaciones
-                                           | pos < agenteN = relacionesAgenteN (tail relaciones) agenteN (pos+1)
+                                         | pos < agenteN = relacionesAgenteN (tail relaciones) agenteN (pos+1)
 
 --Recibe una lista de relaciones de un agente y devuelve su relacion con otro agente
 relacionAgenteNM :: Set Relacion -> Agente -> Integer -> Relacion
 relacionAgenteNM relacionesN agente pos | pos == agente = head relacionesN
                                         | pos < agente = relacionAgenteNM (tail relacionesN) agente (pos+1)
 
---2. Que dado un agente, el n´umero total de agentes del sistema y un estado determinado, indique 
+--2. Que dado un agente, el n´umero total de agentes del sistema y un estado determinado, indique
 --el conjunto de agentes enemigos.
 enemigos :: Agente -> Integer -> Estado -> Set Agente
 enemigos agente cantidadAgentes estado | not (agentePertenece estado agente) = estado
                                        | otherwise = quitarAgentes agentes estado
                                          where agentes = [1..cantidadAgentes]
-                 
+
 --Auxiliares:
 --Recibe una lista de agentes y un agente, y devuelve si el agente pertence o no a la lista
 agentePertenece :: Set Agente -> Agente -> Bool
@@ -33,7 +33,7 @@ agentePertenece estado agente | head estado == agente = True
 quitarAgentes :: Set Agente -> Set Agente -> Set Agente
 quitarAgentes [] _ = []
 quitarAgentes (headAgentes:tailAgentes) estado | not (agentePertenece estado headAgentes) = headAgentes : quitarAgentes tailAgentes estado
-                             | otherwise = quitarAgentes tailAgentes estado
+                                               | otherwise = quitarAgentes tailAgentes estado
 
 --3. Que dado un agente, devuelve su frustracion que es la suma de los valores de su relación con cada uno de los
 --agentes del otro bando.
@@ -49,7 +49,7 @@ sumatoriaFrustracion relacionesN enemigos = relacionAgenteNM relacionesN (head e
 
 --4. Que dado las relaciones y un estado, devuelve la energia total del sistema, es decir, la suma de las frustraciones
 --de cada agente
---Como las relaciones son simétricas, la suma de frustraciones de todos los agentes de un bando es igual a la suma de 
+--Como las relaciones son simétricas, la suma de frustraciones de todos los agentes de un bando es igual a la suma de
 -- frustraciones de los agentes del otro. Luego, calculamos una y la duplicamos para la enemigos total
 energia :: Relaciones -> Estado -> Energia
 energia relaciones estado = 2 * (energiaUnBando relaciones estado estado)
@@ -59,11 +59,11 @@ energia relaciones estado = 2 * (energiaUnBando relaciones estado estado)
 -- devuelve la suma de las frustraciones de todos los agentes del estado.
 energiaUnBando :: Relaciones -> Estado -> Set Agente -> Energia
 energiaUnBando _ _ [] = 0
-energiaUnBando relaciones estado (headAgentes:tailAgentes) = frustracion headAgentes relaciones estado + energiaUnBando relaciones estado tailAgentes 
+energiaUnBando relaciones estado (headAgentes:tailAgentes) = frustracion headAgentes relaciones estado + energiaUnBando relaciones estado tailAgentes
 
 --5. Que dado un agente y un estado, cambia al agente de bando
 adyacente :: Agente -> Estado -> Estado
-adyacente agente estado | agentePertenece estado agente = quitarUnAgente agente estado 
+adyacente agente estado | agentePertenece estado agente = quitarUnAgente agente estado
                         | otherwise = agente : estado
 
 --No se si es mas eficiente la funcion 1 o la 2, porque:
@@ -72,7 +72,7 @@ adyacente agente estado | agentePertenece estado agente = quitarUnAgente agente 
 -- si el largo es igual es porque no pertenecia y entonces lo agrego pero no se como funciona la funcion length de haskell asi que no se si evaluar el length es mas rapido... se los dejo
 adyacente2 :: Agente -> Estado -> Estado
 adyacente2 agente estado | length estadoSinAgente == length estado = agente : estado
-                         | otherwise = estadoSinAgente 
+                         | otherwise = estadoSinAgente
                           where estadoSinAgente = (quitarUnAgente agente estado)
 
 --Auxiliares:
@@ -82,14 +82,74 @@ quitarUnAgente agente [] = []
 quitarUnAgente agente (headEstado:tailEstado) | headEstado /= agente = headEstado : quitarUnAgente agente tailEstado
                                               | otherwise = tailEstado
 
+
+--
+
+
+
+
+
+
+-- dadas las relaciones t un estado , te dice si ese estado es estable o no
 esEstable :: Relaciones -> Estado -> Bool
-esEstable = undefined
+esEstable relaciones estado | energia relaciones estado <= menorEnergia relaciones estado (todosLosAdyacentes estado) = True
+                            | otherwise = False
+
+--
+--Auxiliares para esEstable:
+--
+--dado un estado, devuelve todos los estados adyacentes de ese estado
+todosLosAdyacentes::Estado -> Set Estado
+todosLosAdyacentes estado = losAdyacentes estado estado
+
+--
+-- auxiliar para todosLosAdyacentes
+{--**NOTA** es la forma que se para poder poner un solo parametro en "todosLosAdyacentes",
+y que se lea mejor en la esEstable , quiza habria q buscar una forma mejor para
+no usar 2 funciones nose.... **NOTA**--}
+
+losAdyacentes :: Estado -> Estado -> Set Estado
+losAdyacentes [] estado2 = []
+losAdyacentes estado estado2 = adyacente (head estado) estado2 : losAdyacentes (tail estado) estado2
+
+--
+
+--dadas las relaciones, el estado y los estados adyacentes,
+--te devuelve la energia del estado con menor energia
+menorEnergia :: Relaciones -> Estado -> Set Estado -> Energia
+menorEnergia relaciones estado listaEstados | length listaEstados == 0 = energia relaciones estado
+                                            | energia relaciones estado <= energia relaciones (head listaEstados) = menorEnergia relaciones estado (tail listaEstados)
+                                            | otherwise = menorEnergia relaciones (head listaEstados) (tail listaEstados)
+--
+{--**NOTA** lo probe solo con los dos ejemplos del pdf y da lo mismo,
+ pero igual hay q ver que no se rompa en ningun lado.....**NOTA**--}
+
+
+
+
+
+
+
 
 estadosPosibles :: Integer -> Set (Set Integer)
 estadosPosibles = undefined
 
 predicciones :: Relaciones -> [(Estado, Energia)]
 predicciones = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -108,14 +168,17 @@ agregarATodas n (h:ls) = (h ++ [n]) : (agregarATodas n ls)
 
 
 
+
+
 --Agunas funciones extra:
---2. Que dado un agente, el n´umero total de agentes del sistema y un estado ORDENADO, indique 
+--2. Que dado un agente, el n´umero total de agentes del sistema y un estado ORDENADO, indique
 --el conjunto de agentes enemigos.
 enemigosConOrden :: Agente -> Integer -> Estado -> Set Agente
 enemigosConOrden agente cantidadAgentes estado | agentePertenece estado agente = agentesEnemigos estado agentes
-                                       | otherwise = estado
-                                         where agentes = [1..cantidadAgentes]
+                                               | otherwise = estado
+                                                where agentes = [1..cantidadAgentes]
 --Si estan ordenados!!!
+
 
 --Auxiliares:
 --Recibe un estado y devuelve el otro bando
